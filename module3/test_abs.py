fe367fe6608e8c063423fbac95d209ca78bc8b7f
andgolubev <andrey.golubev@zyfra.com>
2020-08-31 11:28:04 +0300

    Module 3. Lesson 3. Step 8. ДОбавление теста с оформлением в стиле pytest.

---
+++ module3/test_abs.py
@@ -0,0 +1,8 @@
+# pytest -v --tb=line ./module3/test_abs.py
+
+def test_abs1():
+    assert abs(-42) == 42, "Should be absolute value of a number"
+
+
+def test_abs2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
