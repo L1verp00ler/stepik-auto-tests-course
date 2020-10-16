402816532f7b142e63a2e48fa7f69f78aa82987a
andgolubev <andrey.golubev@zyfra.com>
2020-10-16 14:58:44 +0300

    Module 3. Lesson 5. Step 3. Задание на параметризацию (через явные ожидания).

--- module3/test_param_step3_task.py
+++ module3/test_param_step3_task.py
@@ -3,17 +3,18 @@ import time
 import math
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 
 links = [
     'https://stepik.org/lesson/236895/step/1',
     'https://stepik.org/lesson/236896/step/1',
     'https://stepik.org/lesson/236897/step/1',
-    #'https://stepik.org/lesson/236898/step/1',
-    #'https://stepik.org/lesson/236899/step/1',
-    #'https://stepik.org/lesson/236903/step/1',
-    #'https://stepik.org/lesson/236904/step/1',
-    #'https://stepik.org/lesson/236905/step/1'
+    'https://stepik.org/lesson/236898/step/1',
+    'https://stepik.org/lesson/236899/step/1',
+    'https://stepik.org/lesson/236903/step/1',
+    'https://stepik.org/lesson/236904/step/1',
+    'https://stepik.org/lesson/236905/step/1'
 ]
 
 
@@ -21,7 +22,6 @@ links = [
 def browser():
     print("\nstart browser for test...")
     browser = webdriver.Chrome()
-    browser.implicitly_wait(5)
     yield browser
     print("\nquit browser...")
     browser.quit()
@@ -32,15 +32,16 @@ def test_new_task(browser, link):
     answer = math.log(int(time.time()))
     browser.get(link)
 
-    #button = WebDriverWait(browser, 5).until(
-    #    EC._element_if_visible((By.ID, "verify"))
-    #)
-    #button.click()
+    WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.TAG_NAME, 'textarea'))
+    ).send_keys(str(answer))
 
-    browser.find_element_by_tag_name("textarea").send_keys(str(answer))
     browser.find_element_by_class_name("submit-submission").click()
 
-    message = browser.find_element_by_class_name("smart-hints__hint")
+    message = WebDriverWait(browser, 5).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'smart-hints__hint'))
+    )
+
     assert "Correct!" == message.text, "Некорректный ответ!"
 
-    time.sleep(5)
+    time.sleep(2)
