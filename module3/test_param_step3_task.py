0c23e4e03fd8abcdca76fe61ba57a9f8f08012b0
andgolubev <andrey.golubev@zyfra.com>
2020-10-16 14:15:18 +0300

    Module 3. Lesson 5. Step 3. Задание на параметризацию (через неявные ожидания).

--- module3/test_param_step3_task.py
+++ module3/test_param_step3_task.py
@@ -9,48 +9,38 @@ links = [
     'https://stepik.org/lesson/236895/step/1',
     'https://stepik.org/lesson/236896/step/1',
     'https://stepik.org/lesson/236897/step/1',
-    'https://stepik.org/lesson/236898/step/1',
-    'https://stepik.org/lesson/236899/step/1',
-    'https://stepik.org/lesson/236903/step/1',
-    'https://stepik.org/lesson/236904/step/1',
-    'https://stepik.org/lesson/236905/step/1'
-]
-
-linkss = [
-    'https://stepik.org/lesson/236895/step/1'
+    #'https://stepik.org/lesson/236898/step/1',
+    #'https://stepik.org/lesson/236899/step/1',
+    #'https://stepik.org/lesson/236903/step/1',
+    #'https://stepik.org/lesson/236904/step/1',
+    #'https://stepik.org/lesson/236905/step/1'
 ]
 
 
 @pytest.fixture(scope="function")
 def browser():
+    print("\nstart browser for test...")
     browser = webdriver.Chrome()
+    browser.implicitly_wait(5)
     yield browser
+    print("\nquit browser...")
     browser.quit()
 
 
 @pytest.mark.parametrize('link', links)
 def test_new_task(browser, link):
-    #browser = webdriver.Chrome()
-
-    browser.get(link)
     answer = math.log(int(time.time()))
+    browser.get(link)
 
     #button = WebDriverWait(browser, 5).until(
     #    EC._element_if_visible((By.ID, "verify"))
     #)
     #button.click()
 
-    time.sleep(5)
-
     browser.find_element_by_tag_name("textarea").send_keys(str(answer))
-
-    button = browser.find_element_by_class_name("submit-submission")
-    button.click()
-    time.sleep(2)
+    browser.find_element_by_class_name("submit-submission").click()
 
     message = browser.find_element_by_class_name("smart-hints__hint")
     assert "Correct!" == message.text, "Некорректный ответ!"
 
     time.sleep(5)
-
-    #browser.quit()
